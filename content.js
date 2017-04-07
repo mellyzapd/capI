20f22ac12bdfe29c2f9eee495704acb7c67ef6aa
mellyzapd <mellyzapd@gmail.com>
2017-04-07 07:59:03 +0100

    new comment
    new comment

--- content.js
+++ content.js
@@ -1,16 +1,4 @@
-// content.js
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-      console.log(firstHref);
-
-      // This line is new!
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
-    }
-  }
-);
+//content.js
 
 $( "input").keydown(function(e) {
 	 
